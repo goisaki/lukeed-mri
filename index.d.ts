f013b41820f4cff43eed953da534af87dc6be454
Kengo Watanabe <ken5@kwtc.jp>
2024-10-12 10:28:04 +0900

    feat: Improve type inference for Argv

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,21 @@
 type Dict<T> = Record<string, T>;
 type Arrayable<T> = T | T[];
-type Default = Dict<any>;
+/** {@link https://github.com/sindresorhus/type-fest/blob/main/source/simplify.d.ts} */
+type Simplify<T> = { [KeyType in keyof T]: T[KeyType] } & {};
 
-declare function mri<T=Default>(args?: string[], options?: mri.Options): mri.Argv<T>;
+declare function mri<
+	TBoolean extends string = never,
+	TString extends string = never
+>(
+	args?: string[],
+	options?: {
+		boolean?: Arrayable<TBoolean>;
+		string?: Arrayable<TString>;
+		alias?: Dict<Arrayable<string>>;
+		default?: Dict<any>;
+		unknown?(flag: string): void;
+	}
+): mri.Argv<TBoolean, TString>;
 
 declare namespace mri {
 	export interface Options {
@@ -13,9 +26,19 @@ declare namespace mri {
 		unknown?(flag: string): void;
 	}
 
-	export type Argv<T=Default> = T & {
-		_: string[];
-	}
+	export type Argv<
+		TBoolean extends string = never,
+		TString extends string = never
+	> = Simplify<
+		{
+			[P in TBoolean]?: boolean;
+		} & {
+			[P in TString]?: string;
+		} & {
+			[x: string]: any;
+			_: string[];
+		}
+	>;
 }
 
 export = mri;
