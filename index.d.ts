3316fff3a37fd8100ca2b638f167c1cc86979283
Luke Edwards <luke.edwards05@gmail.com>
2024-10-12 01:35:00 -0700

    fix: infer types w/o sig break

--- index.d.ts
+++ index.d.ts
@@ -1,44 +1,54 @@
 type Dict<T> = Record<string, T>;
-type Arrayable<T> = T | T[];
-/** {@link https://github.com/sindresorhus/type-fest/blob/main/source/simplify.d.ts} */
-type Simplify<T> = { [KeyType in keyof T]: T[KeyType] } & {};
+type Prettify<T> = { [K in keyof T]: T[K] } & {};
 
 declare function mri<
-	TBoolean extends string = never,
-	TString extends string = never
+	T = void,
+	const O extends mri.Options = mri.Options,
 >(
 	args?: string[],
-	options?: {
-		boolean?: Arrayable<TBoolean>;
-		string?: Arrayable<TString>;
-		alias?: Dict<Arrayable<string>>;
-		default?: Dict<any>;
-		unknown?(flag: string): void;
-	}
-): mri.Argv<TBoolean, TString>;
+	options?: O,
+): mri.Argv<T, O>;
 
 declare namespace mri {
 	export interface Options {
-		boolean?: Arrayable<string>;
-		string?: Arrayable<string>;
-		alias?: Dict<Arrayable<string>>;
+		string?: string | string[];
+		boolean?: string | string[];
+		alias?: Dict<string | string[]>;
 		default?: Dict<any>;
 		unknown?(flag: string): void;
 	}
 
 	export type Argv<
-		TBoolean extends string = never,
-		TString extends string = never
-	> = Simplify<
-		{
-			[P in TBoolean]?: boolean;
-		} & {
-			[P in TString]?: string;
-		} & {
-			[x: string]: any;
-			_: string[];
-		}
+		T = void,
+		O extends Options = Options,
+	> =
+		& { _: string[] }
+		& (
+			T extends void ? Infer<O> : T
+		);
+
+	// ---
+
+	type Infer<O extends Options> = Prettify<
+		& Defaults<O>
+		& read<O["string"], string>
+		& read<O["boolean"], boolean>
 	>;
+
+	type Defaults<O extends Options> = {
+		-readonly [K in keyof O["default"]]: toType<O["default"][K]>;
+	};
+
+	// remove constant value type
+	type toType<V> = V extends number ? number
+		: V extends boolean ? boolean
+		: V extends string ? string
+		: unknown;
+
+	type read<K extends undefined | string | string[], V> = K extends string
+		? read<[K], V>
+		: K extends string[] ? { [_ in K[number]]?: V }
+		: never;
 }
 
 export = mri;
